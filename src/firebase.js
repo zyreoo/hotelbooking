fe817c4476145aaf08a326e4d851732a272d69ef
zyreoo <simone.marton89@gmail.com>
2025-08-07 17:09:32 +0300

    create room/ db configuration

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDef9tV1YwMgc4ZrBCZ_NkuBy6g10C0-Co",
-  authDomain: "hotelbooking-304d9.firebaseapp.com",
-  projectId: "hotelbooking-304d9",
-  storageBucket: "hotelbooking-304d9.firebasestorage.app",
-  messagingSenderId: "370703822798",
-  appId: "1:370703822798:web:657118ab0b9a78662f859f",
-  measurementId: "G-6KSE1WV6GV"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
